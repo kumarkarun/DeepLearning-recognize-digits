402a09a70c6bea92e77e4f88dff599da7dba0ddf
karun kumar <Karun@karuns-mbp.home>
2018-01-27 08:22:48 -0500

    Train and test the accuracy of network

---
+++ src/test.py
@@ -0,0 +1,24 @@
+"""
+mnist_loader
+~~~~~~~~~~~~
+
+A library to load the MNIST image data.  For details of the data
+structures that are returned, see the doc strings for ``load_data``
+and ``load_data_wrapper``.  In practice, ``load_data_wrapper`` is the
+function usually called by our neural network code.
+"""
+
+#### Libraries
+# Standard library
+import cPickle
+import gzip
+import network
+# Third-party libraries
+import numpy as np
+
+import mnist_loader
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 0.1, test_data=test_data)
